1c89b780a8951aade7807e61f590a06d785d39bc
paulafranceschini <paula.franceschini@gmail.com>
2020-11-27 09:24:17 -0300

    ALTERACOES

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 from flask import Flask, jsonify, json;
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
